1cca2516b494e4583526bf9846c0931d7712693c
fadliselaz <fadliselaz@gmail.com>
2018-08-01 23:16:20 +0700

    tambah variable nama

--- codepolitan/index.php
+++ codepolitan/index.php
@@ -2,7 +2,15 @@
 
 echo "php is fun.. pffffff";
 echo "<br> ";
+
 echo 1000 + 2000;
+$nama = ["fadli","tammy","budi"];
+echo "<br> ";
+
+$nama = "fadli";
+echo $nama;
+
+//ini telah di ubah...
 
 
 // untuk mengeyahui versi php silakan gunakan func berikut
